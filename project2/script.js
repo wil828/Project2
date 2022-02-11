6d70fae96022e81435c58bb902981e2cd2126e12
william <williamw8828@gmail.com>
2022-02-11 11:25:38 -0500

    added script.js with pseudocode

---
+++ project2/script.js
@@ -0,0 +1,8 @@
+// Create app object (pokemonApp)
+// Create initialization method (pokemonApp.init)
+// Call the init method
+// Create a method which will request information for the API (pokemonApp.getPokemon)
+// Build a method that will display a random picture of a pokemon on front end (pokemonApp.chosenPokemon)
+// Build a method that will compare user input to displayed pokemon and append user input to list (pokemonApp.checkAnswer)
+// Build a method that will display pokemon img and name on a pop up upon correct guess (pokemonApp.correctAnswer)
+// Restarts when the user clicks “Play again” (eventListener)
