f9e5c14212d59f8de5ab85857b435c0dc47874e0
william <williamw8828@gmail.com>
2022-02-12 01:39:29 -0500

    fetched from pokeApi and connected script.js to HTML

--- project2/script.js
+++ project2/script.js
@@ -1,8 +1,40 @@
 // Create app object (pokemonApp)
+const pokemonApp = {};
+
+// creating an apiURL
+
+pokemonApp.apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+
 // Create initialization method (pokemonApp.init)
-// Call the init method
+pokemonApp.init = () => {
+    pokemonApp.getPokemon();
+}
+
 // Create a method which will request information for the API (pokemonApp.getPokemon)
+pokemonApp.getPokemon = () => {
+
+    const url = new URL(pokemonApp.apiURL);
+    url.search = new URLSearchParams({
+        limit : '151',
+        // offset : '40'
+    });
+    // console.log(url);
+    fetch(url)
+    .then (function(apiResponse) {
+        return apiResponse.json()
+    })
+    
+    .then (function(jsonResponse) {
+        console.log(jsonResponse)
+    })
+
+};
+
 // Build a method that will display a random picture of a pokemon on front end (pokemonApp.chosenPokemon)
 // Build a method that will compare user input to displayed pokemon and append user input to list (pokemonApp.checkAnswer)
 // Build a method that will display pokemon img and name on a pop up upon correct guess (pokemonApp.correctAnswer)
 // Restarts when the user clicks “Play again” (eventListener)
+
+
+// Call the init method
+pokemonApp.init();
